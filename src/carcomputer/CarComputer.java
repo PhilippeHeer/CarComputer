efb07f538a84ff24a152968866b2d7beb0f7a958
PhilippeHeer <philippe.heer@epfl.ch>
2014-06-08 00:26:17 +0200

    nice interface

--- src/carcomputer/CarComputer.java
+++ src/carcomputer/CarComputer.java
@@ -17,6 +17,9 @@ public class CarComputer {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Window newJFrame = new Window();
+        newJFrame.setVisible(true);
     }
     
 }
